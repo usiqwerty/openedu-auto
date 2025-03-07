56f629e64b03ef112ab1c5855713acd93292c727
usiqwerty <benlogin1@protonmail.com>
2025-03-07 18:40:06 +0500

    feat: description interval

--- images/image_describer.py
+++ images/image_describer.py
@@ -1,10 +1,20 @@
+import time
 from abc import ABC, abstractmethod
+import datetime
 
 
 class ImageDescriber(ABC):
+    last_described = 0
+    interval_sec = 5
 
     def describe(self, url: str) -> str:
-        return f"Изображение: {self.get_description(url)}"
+        now = datetime.datetime.now().timestamp()
+        delta = now - self.last_described
+        if delta < self.interval_sec:
+            time.sleep(delta)
+        description = self.get_description(url)
+        self.last_described = now
+        return f"Изображение: {description}"
 
     @abstractmethod
     def get_description(self, url: str)->str:
