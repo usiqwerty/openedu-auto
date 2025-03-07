9175be857ed2d095ec11bf85c16990971fc15165
usiqwerty <benlogin1@protonmail.com>
2025-03-07 16:29:50 +0500

    something

--- autosolver.py
+++ autosolver.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import traceback
 
 from cached_requests import CacheContext
 from images.image_describer import ImageDescriber
@@ -30,33 +31,35 @@ class OpenEduAutoSolver:
             api.parse_and_save_sequential_block(url)
 
             for blkid, block in api.iterate_incomplete_blocks():
-                self.solve_block(api, blkid, block)
+                self.solve_block(api, blkid, block, course_id)
 
-    def solve_block(self, api: OpenEdu, blkid: str, block: VerticalBlock):
+    def solve_block(self, api: OpenEdu, blkid: str, block: VerticalBlock, course_id: str):
         logging.debug(blkid)
         logging.debug(f"Block '{block.title}' (complete={block.complete}) of type '{block.type}'")
         if block.type == 'other':
+            return
             print(blkid, block)
             api.tick_page(blkid)
             time.sleep(5)
             api.publish_completion(blkid)
         elif block.type == "problem":
             for problem in api.get_problems(blkid):
+                answers = {}
                 for question in problem:
                     print(question.query())
-                    continue
                     try:
-                        answers = {}
-                        k, v = solver.solve(question)
-                        answers[k] = v
-                        quest_id = api.extract_quest_id(k)
-                        new_block_id = f"block-v1:{course_id}+type@problem+block@{quest_id}"
-
-                        got, total = api.problem_check(new_block_id, answers)
-                        logging.info(f"Solved ({got}/{total})")
-                        if got < total:
-                            logging.critical("Wrong answer!")
-                            exit(1)
+                        input_id, input_value = self.solver.solve(question)
+                        answers[input_id] = input_value
                     except KeyError as e:
                         print("Error:", e)
+                        traceback.print_exc()
+                        exit(1)
+                quest_id = api.extract_quest_id(input_id)
+                print(f"{answers=}")
+                new_block_id = f"block-v1:{course_id}+type@problem+block@{quest_id}"
+                got, total = api.problem_check(new_block_id, answers)
+                logging.info(f"Solved ({got}/{total})")
+                if got < total:
+                    logging.critical("Wrong answer!")
+                    exit(1)
             return
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import logging
 
+from autosolver import OpenEduAutoSolver
+from solvers.mistral_solver import MistralSolver
+from solvers.openrouter.gemini_solver import GeminiSolver
+
 logging.getLogger().setLevel(logging.DEBUG)
+solver = GeminiSolver()
+describer = None
 
-
+course_id = "spbstu+PHYLOS+spring_2024_4"
+app = OpenEduAutoSolver(solver, describer)
+app.solve_course(course_id)
 # course_id = "urfu+HIST+spring_2025"
-# course_id = "spbstu+PHYLOS+spring_2024_4"
