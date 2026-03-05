bcd52533a39ca1665dcb19f390745cecc7169e02
usiqwerty <benlogin1@protonmail.com>
2026-03-05 12:56:01 +0500

    feat: check json validity

--- solvers/llm_solver.py
+++ solvers/llm_solver.py
@@ -14,6 +14,11 @@ from openedu.questions.select import SelectQuestion
 from solvers.abstract_solver import AbstractSolver
 
 
+def striplines(raw_result: str) -> str:
+    "Call .strip() on each line and then join back"
+    return '\n'.join([result_line.strip() for result_line in raw_result.split('\n')])
+
+
 class LLMSolver(AbstractSolver, ABC):
     cache_fn: str
     last_described = 0
@@ -36,10 +41,10 @@ class LLMSolver(AbstractSolver, ABC):
         self.save_cache(self.__cache)
 
     @abstractmethod
-    def make_gpt_request(self, query) -> str:
+    def make_gpt_request(self, query, *, sysprompt, _json) -> str:
         pass
 
-    def get_answer(self, query) -> str:
+    def get_answer(self, query, *, sysprompt=None, json=False) -> str:
         if query not in self.__cache:
             logging.debug("Question was not in cache")
 
@@ -47,8 +52,8 @@ class LLMSolver(AbstractSolver, ABC):
             delta = now - self.last_described
             if delta < self.interval_sec:
                 time.sleep(delta)
-            raw_result = self.make_gpt_request(query).strip()
-            result = '\n'.join([result_line.strip() for result_line in raw_result.split('\n')])
+            raw_result = self.make_gpt_request(query, sysprompt=sysprompt, _json=json).strip()
+            result = striplines(raw_result)
             self.cache_set(query, result)
             self.last_described = now
         else:
@@ -97,6 +102,6 @@ class LLMSolver(AbstractSolver, ABC):
         return question.compose(res)
 
     def solve_new_match(self, question: NewMatchQuestion):
-        raw = self.get_answer(question.query())
+        raw = self.get_answer(question.query(), sysprompt="Ответ должен содержать валидный JSON", json=True)
         json_data = json.loads(raw)
         return question.compose(json_data)
