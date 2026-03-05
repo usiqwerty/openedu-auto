f8bb754d5be7bf786b83b6d15531ec5d06495d01
usiqwerty <benlogin1@protonmail.com>
2026-03-05 15:47:19 +0500

    fix: didn't raise exception if no similar option

--- solvers/utils.py
+++ solvers/utils.py
@@ -21,6 +21,8 @@ def get_ans_id(answers: list[tuple[str, str]], answer: str):
 def get_ans_id_best(answers: list[tuple[str, str]], answer: str):
     assert isinstance(answer, str)
     idx = get_similar_index(answer, [ans for ans, aid in answers])
+    if idx is None:
+        raise NoSolutionFoundError(f"'{answer}' was not present in options {answers}")
     ans, aid = answers[idx]
     return aid
 
